0eaf64e09d00954f36e105cd465be5c20d3c3315
Antonio Martinho Dias <130603581+antnmrtnds@users.noreply.github.com>
2025-10-24 09:32:58 +0100

    results phrase change

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -23,7 +23,7 @@ const Results = () => {
       className="relative w-full max-w-[1440px] mx-auto px-[40px] md:px-[60px] lg:px-[80px] py-20 flex items-center justify-center"
     >
       <motion.div 
-        className="text-[24px] md:text-[36px] lg:text-[52px] font-work-sans text-white leading-normal text-center"
+        className="text-[24px] md:text-[32px] lg:text-[32px] font-work-sans text-white font-light leading-normal text-left"
         initial={{ opacity: 0, y: 30 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.5 }}
@@ -36,7 +36,11 @@ const Results = () => {
           viewport={{ once: true }}
           transition={{ duration: 0.6, delay: 0.2 }}
         >
-          Boost your sales by promoting your properties with hight quality communication that amplifies reach and accelerates deals.
+          <span>Boost your sales by promoting your properties with high quality communication that </span>
+          <span className="font-normal italic text-primary-accent">amplifies reach</span>
+          <span> and </span>
+          <span className="font-normal italic text-primary-accent">accelerates deals</span>
+          <span>.</span>
         </motion.p>
       </motion.div>
     </section>
